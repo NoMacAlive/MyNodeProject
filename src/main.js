0905e6a2430daff564a7b35eee6e6e13a911d787
XiaoDangJia <zhuguangya98@gmail.com>
2020-02-09 14:11:04 +1300

    found the way to use the router and made a new page

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 导入组件库
+import ElementUI from 'element-ui'
+// 导入组件相关样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 配置Vue插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import CaseStudy from '@/components/CaseStudy'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/casestudy',
+      name: 'CaseStudy',
+      component: CaseStudy
     }
   ]
 })
