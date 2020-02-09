6e9daabd69e1a7336b34ae6f7c2a20780911d8ed
XiaoDangJia <zhuguangya98@gmail.com>
2020-02-09 15:31:15 +1300

    added the side bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,42 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-container style="height: 1000px">
+        <el-aside>
+        <div style="background-color: darkgrey" >
+          <el-menu default-active="1-4-1" class="el-menu-vertical-demo">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-s-home"></i>
+                <span slot="title">Home</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1"><span class="home">Introduction</span></el-menu-item>
+                <el-menu-item index="1-2"><span class="home">GuideLines</span></el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">About Us</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-document"></i>
+              <span slot="title">Contact Us
+
+              </span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">FeedBack</span>
+            </el-menu-item>
+          </el-menu>
+        </div>
+        </el-aside>
+
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+    </el-container>
+
   </div>
 </template>
 
@@ -17,6 +53,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  min-height: 400px;
+  height: 1000px;
+  /* background-color: #409EFF; */
+}
+.home {
+  font:bold;
+}
+
 </style>
